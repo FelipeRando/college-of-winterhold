e2389c8c4532c779b1aec2ccf8704e598da9f27f
FelipeRando <feliperando2@gmail.com>
2018-04-24 16:20:46 -0300

    create script watch-directory-for-changes-then-do-something.sh
    added script watch-directory-for-changes-then-do-something.sh

---
+++ watch-directory-for-changes-then-do-something.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#inotify-tools is a tool you can use (a.o.) to watch a defined folder and take (defined) actions if a new file is added to the folder.
+
+#Install inotify-tools
+#sudo apt-get install inotify-tools
+#Paste the script below in an empty file, set the folder to watch ("/path/to/your/downloadfolder"), define the command it should run ("command-to-run-your-application") safe it as notifyscript.sh,
+#make it executable for convenience reasons, and add it to your startup applications: 
+#(Preferences > Default applications for LXSession, then choose "Autostart". Add the command:
+#/path/to/notifyscript.sh
+
+while true
+do
+    fileName=$(inotifywait -r -e create /path/to/your/downloadfolder | sed -r 's/^.*CREATE(,ISDIR)*\s+(.*)$/\2/g')
+    command-to-run-your-application
+done
